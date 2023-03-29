cd806ab023e809ca97cc1753a6cc4ee2201b2995
cdw18 <90944173+cdw18@users.noreply.github.com>
2023-03-29 18:33:42 -0400

    Changed model class to be similar to CNN model structure in base model

--- DP-PATE/model.py
+++ DP-PATE/model.py
@@ -2,24 +2,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Model(nn.Module):
-    """Class used to initialize model of student/teacher"""
+class CNN(nn.Module):
+    """Class used to initialize CNN model of student/teacher"""
 
-    def __init__(self):
-
-        super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4 * 4 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
+    def __init__(self, num_classes):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3)
+        self.conv2 = nn.Conv2d(64, 32, 3)
+        self.fc1 = nn.Linear(32 * 14 * 14, 64*num_classes)
+        self.fc2 = nn.Linear(64*num_classes, num_classes)
 
     def forward(self, x):
-
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4 * 4 * 50)
+        x = x.view(-1, 32 * 14 * 14)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
